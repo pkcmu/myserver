413c332e0628596d9515cfc97113a28b2ff4d785
piyavach <popthebreaker@hotmail.com>
2024-09-05 11:39:20 +0700

    update Test1

---
+++ src/Test1.ts
@@ -0,0 +1,10 @@
+import { Utils } from "./Utils";
+
+//test add
+if(Utils.add(1,2) === 3)
+{
+    console.log(0);
+}
+else{
+    console.log(1);
+}
---
+++ src/Utils.ts
@@ -0,0 +1,9 @@
+function helloworld():string{
+    return "hello world";
+}
+
+function add(a:number,b:number):number{
+    return a+b;
+}
+
+export const Utils = {add}
