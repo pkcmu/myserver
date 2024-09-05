4e8fb3bed8b96bb8d2dddbee985ea493bd7c33f7
piyavach <popthebreaker@hotmail.com>
2024-09-05 11:07:31 +0700

    test2

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const Utils_1 = require("./Utils");
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send(Utils_1.Utils.add(1, 2) + "");
 });
 app.listen(port, () => {
     console.log(`Server is running ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 
+import { Utils } from './Utils';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {    
-      res.send('Hello, World!');
+      res.send(Utils.add(1,2)+"");
 });
 
 app.listen(port, () => {    
