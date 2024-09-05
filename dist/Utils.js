6d9dca345de0ff4726b2ffe5884ce3bd341b2f0a
piyavach <popthebreaker@hotmail.com>
2024-09-05 11:09:31 +0700

    test3

---
+++ dist/Utils.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Utils = void 0;
+function helloworld() {
+    return "hello world";
+}
+function add(a, b) {
+    return a + b;
+}
+exports.Utils = { add };
