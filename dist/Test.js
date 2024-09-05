c1ebdb030263e47755332afff53fc03587cec7e5
piyavach <popthebreaker@hotmail.com>
2024-09-05 11:30:56 +0700

    Create Test.js

---
+++ dist/Test.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Utils_1 = require("./Utils");
+if (Utils_1.Utils.add(1, 2) === 3) {
+    console.log(0);
+}
+else {
+    console.log(1);
+}
