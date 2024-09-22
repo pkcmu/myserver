300a21e3fbb9b7faaa60808f653b2f109572670b
piyavach <popthebreaker@hotmail.com>
2024-09-22 22:04:32 +0700

    update

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,10 +1,51 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
+const axios_1 = __importDefault(require("axios"));
 //test add
 if (Utils_1.Utils.add(1, 2) === 3) {
     console.log(0);
 }
 else {
     console.log(1);
+}
+//test add
+function test_json_placeholder() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const data = {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        };
+        try {
+            const response = yield axios_1.default.post('https://jsonplaceholder.typicode.com/users', data);
+            const correct_result = {
+                id: 101,
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            };
+            const keys = Object.keys(correct_result);
+            keys.forEach(key => {
+                if (correct_result[key] !== response[key]) {
+                    console.log(1);
+                }
+            });
+        }
+        catch (error) {
+            console.log(1);
+        }
+    });
 }
--- src/Test1.ts
+++ src/Test1.ts
@@ -1,10 +1,41 @@
 import { Utils } from "./Utils";
+import axios, { AxiosResponse } from "axios";
 
 //test add
-if(Utils.add(1,2) === 3)
-{
+if (Utils.add(1, 2) === 3) {
     console.log(0);
 }
-else{
+else {
     console.log(1);
 }
+
+//test add
+async function test_json_placeholder() {
+    const data: any = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    };
+    try {
+        const response:any = await axios.post('https://jsonplaceholder.typicode.com/users', data);
+
+        const correct_result = {
+            id: 101,
+            title: 'foo',
+            body: 'bar',
+            userId: 1
+        } as any
+
+        const keys:string[] = Object.keys(correct_result);
+        keys.forEach(key=>{
+            if(correct_result[key] !== response[key])
+            {
+                console.log(1)
+            }
+        })
+    } catch (error) {
+        console.log(1)
+    }
+}
+
+
